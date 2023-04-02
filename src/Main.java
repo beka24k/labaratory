22aac15d156a8eb757c6b87d8a8c5252e4e5306c
beka24k <110625229+beka24k@users.noreply.github.com>
2023-04-02 13:33:25 +0600

    labaratory 2 problem

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {10,1,32,3,45};
-        int min=findMin(arr,arr.length);
-        System.out.println(min);
+        int[] arr = {3, 2, 4, 1};
+        double sum=findAvg(arr,arr.length);
+        double nofn=arr.length;
+        System.out.println(sum/nofn);
     }
-    public static int findMin(int[] arr, int n){
-        if (n ==1){
-            return arr[0];
-        }else{
-            int min=findMin(arr,n-1);
-            return Math.min(min,arr[n-1]);
 
+    public static int findAvg(int[] arr, int n) {
+        if (n == 0) {
+            return 0;
+        } else {
+            int sum;
+            sum=arr[n-1]+findAvg(arr,n-1);
+            return sum;
         }
     }
 }
