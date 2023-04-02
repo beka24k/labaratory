367e3f054a3666be9dd5db27fea9a762b91603cf
beka24k <110625229+beka24k@users.noreply.github.com>
2023-04-02 17:45:59 +0600

    labaratory 5 problem

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        System.out.println(factorial(number));
+        System.out.println("sum of fibonacci numbers- "+sumFibanacci(number+1));
+        System.out.println("5 problem solution- "+fibanacci(number));
     }
-public static int factorial(int number){
-        if(number==0){
-            return 1;
+
+
+public static int sumFibanacci(int number){
+        if (number==0){
+            return 0;
         }else{
-            return number*factorial(number-1);
+            return fibanacci(number)+fibanacci(number-1);
         }
 }
-}
+    public static int fibanacci(int number) {
+        if (number <= 1) {
+            return number;
+        }else{
+                return fibanacci(number - 1) + fibanacci(number - 2);
+            }
+        }
+    }
+
+
