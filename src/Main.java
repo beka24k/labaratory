36abb16918219132eb80324b9af8a59aebea2034
beka24k <110625229+beka24k@users.noreply.github.com>
2023-04-04 11:13:31 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,13 @@ public class Main {
     }
 
     //using data to make function for solution
-    public static int isDigit(String number, int counter) {
+    public static String isDigit(String number, int counter) {
         char c = number.charAt(counter);
         if (counter == 0) {//base case
-            return 1;
+            return "Yes";
         }
         if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z') {//checker if the function will found letter witout
-            return 0;
+            return "No";
         } else { //recursion case
             return isDigit(number, counter - 1);//recursion for making easier problem
         }
